5afd49a2f9bdc131d00f4ab9157d464c4f51b6a7
eredden <redden.evan@gmail.com>
2025-01-18 19:53:32 -0500

    added todo notes for future

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,12 @@
 int main(void) {
     // TODO: Create data file that stores questions and answers
     // and import the data into this program when loaded.
+
+    // TODO: Make the size of this array adjust with the amount of questions
+    // that are loaded into this program.
+    Answer answers[3];
+
+    // TODO: Ask imported questions iteratively.
     Question question = Question(
         "What should the government do to help the poor?",
         (Answer[MAX_ANSWER_COUNT]) {
